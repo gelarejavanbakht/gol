2f245d6faf25b3a692a1f569f4f54df9446dff4b
javanbakhtgelareh <javanbakhtgelareh@gmail.com>
2024-12-05 07:52:58 +0330

    version-369-product

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      mobile: "0px",
+      tablet: "768px",
+      ipad: "1024px",
+      desktop: "1280px",
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
+
+
